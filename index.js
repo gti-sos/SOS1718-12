2ac6efb32fcf8c3a73509c2bfc50d7a96738d74b
Manuel Jesus Torres Rincon <mjtr1995@gmail.com>
2018-03-09 01:15:18 +0000

    s

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require("express");
+var path = require("path");
+
 var app = express();
 
 app.get("/hello",(req,res)=>{
     res.send("Hello World");
 });
 app.listen(process.env.PORT);
+
+app.use("/", express.static(path.join(__dirname,"public")));
