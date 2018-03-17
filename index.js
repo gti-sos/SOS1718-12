3579a7590fb48d94b546271d9411e8b2a4831ecc
Manuel Jesus Torres Rincon <mjtr1995@gmail.com>
2018-03-17 19:58:22 +0000

    api manuel ready

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
+
 
 var app = express();
+app.use(bodyParser.json());
 
-app.get("/hello",(req,res)=>{
+
+app.get("/hello", (req, res) => {
     res.send("Hello World");
 });
 
 
-app.use("/", express.static(path.join(__dirname,"public")));
+app.use("/", express.static(path.join(__dirname, "public")));
 
 app.listen(process.env.PORT);
+
+
+/*********API MANUEL CON NEDB*********/
+
+var rape = require("./public/rape-manager/v1/rape.js");
+
+/****Gets***/
+app.get("/api/v1/rape-stats/loadInitialData", rape.getInitialData);
+app.get("/api/v1/rape-stats", rape.getAllData);
+app.get("/api/v1/rape-stats/:name/:year", rape.getSingleDataNameYear);
+app.get("/api/v1/rape-stats/:name", rape.getData);
+
+/**Post**/
+app.post("/api/v1/rape-stats", rape.postDataGroup);
+app.post("/api/v1/rape-stats/:name", rape.postDenied);
+app.post("/api/v1/rape-stats/:name/:year", rape.postDenied);
+
+/***Put****/
+app.put("/api/v1/rape-stats", rape.putDenied);
+app.put("/api/v1/rape-stats/:name", rape.putDenied);
+app.put("/api/v1/rape-stats/:name/:year", rape.putSingleData);
+
+/***Delete**/
+app.delete("/api/v1/rape-stats", rape.deleteAll);
+//app.delete("/api/v1/rape-stats/:country" ,rape.deleteData);
+app.delete("/api/v1/rape-stats/:name/:year", rape.deleteData);
