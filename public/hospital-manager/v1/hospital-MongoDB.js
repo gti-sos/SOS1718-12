011270c2af4b412e5594589bb7c43bac45ac453b
Veronica <vela.palomo.29@gmail.com>
2018-04-09 10:28:18 +0000

    kj

--- public/hospital-manager/v1/hospital-MongoDB.js
+++ public/hospital-manager/v1/hospital-MongoDB.js
@@ -199,7 +199,7 @@ module.exports.getCollection = (request, response) => {
 };
 
 //GET a un recurso
-module.exports.getRecursoConcreto = (request, response) => {
+module.exports.getRecurso = (request, response) => {
 		var key = request.query.apikey;
 		if (!key) {
 			response.sendStatus(401);
