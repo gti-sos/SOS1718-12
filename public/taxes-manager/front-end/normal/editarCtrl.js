e6f82aae5a8a20e04404e142b25354ebb680b5f3
jose llopis zapata <josellopis90@gmail.com>
2018-04-23 08:21:48 +0000

    cambios en el editarCtrl.js

--- public/taxes-manager/front-end/normal/editarCtrl.js
+++ public/taxes-manager/front-end/normal/editarCtrl.js
@@ -1,51 +1,42 @@
 /*global angular*/
-
+/* global Materialize */
 angular.module("managerApp")
 .controller("editarCtrl",["$scope","$http","$routeParams","$location",function($scope,$http,$routeParams,$location){
     console.log("Edit controller initialized");
-    $scope.url  = "api/v1/taxes-stats/";
+    var api = "/api/v1/taxes-stats";
 
-    function refresh(){
-   $http
-       .get($scope.url + $routeParams.country + "/" + Number($routeParams.year))
-       .then(function successCallback(response) {
-           
-               $scope.updatedTax = response.data[0];
-               console.log("Recurso  encontrado");
-               console.log($routeParams.country);
-               console.log($routeParams.year);
-               
-           }, function errorCallback(response) {
-               console.log("Recurso No encontrado");
-               $scope.updatedTax = [];
+function refresh() {
+            $http.get(api + "/" + $routeParams.country + "/" + $routeParams.year).then(function(response) {
+                console.log(response);
+                $scope.updatedTax = response.data;
+            });
+        }
 
-           });
-}
-    
-    $scope.updateTax = function(updatedTax){
-      
-            $http
-                .put($scope.url  + updatedTax.country + "/" + Number(updatedTax.year) , {
-                    country: updatedTax.country,
-                    year: updatedTax.year,
-                    region: updatedTax.region,
-                    income_group: updatedTax.income_group,
-                    country_code: updatedTax.country_code
-                })
-                .then(function(response) {
-                    console.log("Stat Updated 2");
-                    switch (response.status) {
-                        case 400:
-                            alert("Bad request");
-                            break;
-                        default:
-                            alert("OK");
-                            break;
-                    }
-                    $location.path("/api/v1/taxes-stats");
+  
+        
+         $scope.updateTax= function() {
+            var updatedTaxAux = {};
+            updatedTaxAux.country = $scope.updatedTax.country;
+            updatedTaxAux.year = $scope.updatedTax.year;
+            updatedTaxAux.region = $scope.updatedTax.region;
+            updatedTaxAux.income_group = $scope.updatedTax.income_group;
+            updatedTaxAux.country_code = $scope.updatedTax.country_code;
+            console.log($scope.updatedTax);
 
-                });
+            $http.put(api + "/" + updatedTaxAux.country + "/" + updatedTaxAux.year, updatedTaxAux).then(function(response) {
+                $scope.status = "Status: " + response.status;
+            }, function(response) {
+                switch (response.status) {
+                    case 400:
+                        Materialize.toast('<i class="material-icons">error_outline</i> Make sure to set all atributes!', 2500);
+                        break;
+                    default:
+                        Materialize.toast('<i class="material-icons">error_outline</i> Error getting data!', 2500);
+                        break;
+                }
+            });
         };
+        
        refresh();
     
 }]);
