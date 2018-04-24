01370a1ced56b1fc368c3630d660fd39b7ed97c5
Manuel Jesus Torres Rincon <mjtr1995@gmail.com>
2018-04-24 14:37:20 +0000

    parte manuel quitada

--- public/rape-manager/front-end/normal/rapeEditCtrl.js
+++ public/rape-manager/front-end/normal/rapeEditCtrl.js
@@ -3,48 +3,6 @@
 angular.module("managerApp")
 .controller("rapeEditCtrl",["$scope","$http","$routeParams","$location",function($scope,$http,$routeParams,$location){
     console.log("Edit controller initialized");
-    $scope.url  = "api/v1/rape-stats/";
-
-    function refresh(){
-        $http
-            .get($scope.url +  $routeParams.country + "/" + Number($routeParams.year))
-            .then(function successCallback(response) {
-                    $scope.updateData = response.data[0];
-
-                }, function errorCallback(response) {
-                    console.log("Entra1");
-                    $scope.updateData = [];
-
-                });
-    }
     
-    $scope.actualizaData = function(updateData){
-      
-            $http
-                .put($scope.url  + updateData.country + "/" + Number(updateData.year) , {
-                    country: updateData.country,
-                    year: updateData.year,
-                    ["number-of-rape"]: updateData["number-of-rape"],
-                    rate: updateData.rate,
-                    ["total-since-two-thousand"]: updateData["total-since-two-thousand"]
-                })
-                .then(function(response) {
-                    console.log("Stat Updated 2");
-                    switch (response.status) {
-                        case 400:
-                            alert("Bad request");
-                            break;
-                        default:
-                            alert("OK");
-                            break;
-                    }
-                    $location.path("/api/v1/rape-stats");
-
-                });
-        };
-
-
-
-        refresh();
     
 }]);
