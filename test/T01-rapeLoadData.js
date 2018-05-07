be2a000b7b8bcf7e8ba1833f4ba9913ce9a38c93
Manuel Jesus Torres Rincon <mjtr1995@gmail.com>
2018-05-07 09:17:13 +0000

    ss

--- test/T01-rapeLoadData.js
+++ test/T01-rapeLoadData.js
@@ -1,6 +1,6 @@
 describe('Data is loaded', function () {
 	it('should show 15 data', function (){
-		browser.get('https://personal-sos161709mjtr.c9users.io/#!/api/v2/rape-stats')
+		browser.get('https://sos1718-12.herokuapp.com/#!/api/v2/rape-stats')
 		.then(function (){
 		    
 		    element.all(by.repeater("data in database"))
--- test/T02-rapeCreateData.js
+++ test/T02-rapeCreateData.js
@@ -1,6 +1,6 @@
 describe('Add stats', function() {
     it('should add a new stats', function() {
-      browser.get('https://personal-sos161709mjtr.c9users.io/#!/api/v2/rape-stats')
+      browser.get('https://sos1718-12.herokuapp.com/#!/api/v2/rape-stats')
 
         element.all(by.repeater("data in database")).then(function(initialData) {
             //browser.driver.sleep(2000);
