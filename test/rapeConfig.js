44c665f4481a97dc4221de0402ed3b0c3487880c
Manuel Jesus Torres Rincon <mjtr1995@gmail.com>
2018-05-25 21:11:29 +0000

    feedback manuel lunes28 mayo hecho

--- test/rapeConfig.js
+++ test/rapeConfig.js
@@ -2,7 +2,7 @@ exports.config = {
     
     seleniumAddress: 'http://localhost:8910',
     
-    specs: ['T01-rapeLoadData.js' , 'T02-rapeCreateData.js'],
+    specs: ['T00-rapePostman.js','T01-rapeLoadData.js' , 'T02-rapeCreateData.js'],
     
     capabilities: {
         'browserName' : 'phantom.js'
