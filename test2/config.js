abdc260fd0c7b6db769743519f374fc26d3dc223
jose llopis zapata <josellopis90@gmail.com>
2018-05-28 10:55:58 +0000

    cambios4

---
+++ test2/config.js
@@ -0,0 +1,18 @@
+exports.config = { 
+    
+    seleniumAddress:'http://localhost:8910',
+    
+    specs:['T01-loadData.js','T02-addTaxes.js','T00-Backend.js'],
+    
+    capabilities: {
+        'browserName':'phantomjs'
+    }, params: {
+        host: 'sos1718-12.herokuapp.com',
+        port: '80'
+    }
+
+};
+exports.getTaxesUrl = function() {
+    console.log("https://" + browser.params.host  + "/#!/api/v1/taxes-stats" );
+    return "https://" + browser.params.host  + "/#!/taxes-stats";
+};
