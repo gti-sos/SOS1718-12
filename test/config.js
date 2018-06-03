8c8230028214583a3655f2cfbe77237fc919dd2c
Manuel Jesus Torres Rincon <mjtr1995@gmail.com>
2018-06-03 14:32:17 +0000

    ss

--- test/config.js
+++ test/config.js
@@ -1,12 +1,12 @@
-exports.config = { //exportar objeto de la configuracion del test
+exports.config = {
 
-    seleniumAddress: 'http://localhost:8910', //url donde esta el navegador cargando
+    seleniumAddress: 'http://localhost:8910',
 
     specs: ['T00-rapePostman.js',/* 'T00-Backend.js',*/
         'T01-rapeLoadData.js', 'T02-rapeCreateData.js',
         'T01-loadData.js', 'T02-addTaxes.js'
 
-    ], //test que vamos a cargar
+    ], 
 
     capabilites: { //tipo de navegador que voy a usar
         'browserName': 'phantomjs'
